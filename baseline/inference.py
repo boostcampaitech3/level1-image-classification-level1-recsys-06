8b3e027a412b3732c345f60b7afcde789f60d368
Ho Jin Lee <65278309+ili0820@users.noreply.github.com>
2022-02-25 21:33:31 +0900

    Minor Baseline Fix
    dataset의 choices -> sample
    train, inference의 resize 받는 부분 수정

--- baseline/inference.py
+++ baseline/inference.py
@@ -72,7 +72,7 @@ if __name__ == '__main__':
 
     # Data and model checkpoints directories
     parser.add_argument('--batch_size', type=int, default=1000, help='input batch size for validing (default: 1000)')
-    parser.add_argument('--resize', type=tuple, default=(96, 128), help='resize size for image when you trained (default: (96, 128))')
+    parser.add_argument('--resize', type=int, default=(96, 128), help='resize size for image when you trained (default: (96, 128))')
     parser.add_argument('--model', type=str, default='BaseModel', help='model type (default: BaseModel)')
 
     # Container environment
