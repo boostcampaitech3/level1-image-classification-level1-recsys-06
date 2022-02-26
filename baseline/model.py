60c02685c5f0cf3e1e858423c908a44d4a073f22
DESKTOP-OVL581Q\pc <ravage13@naver.com>
2022-02-26 14:30:58 +0900

    update model.py
    - densenet121 수정
    - densenet161 수정
    - densenet191 수정
    - densenet201 수정
    
    - efficientnet_b0~b7 추가
    - googlenet 추가

--- baseline/model.py
+++ baseline/model.py
@@ -4,316 +4,134 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
-
+from efficientnet_pytorch import EfficientNet
 
 # Custom Model Template
 
 class densenet121(nn.Module):
     def __init__(self, num_classes=18):
-        super(densenet121, self).__init__()
+        super().__init__()
+
         self.model = torchvision.models.densenet121(pretrained=True)
-        num_init_features = 64
-        block_config=(6, 12, 24, 16)
-        bn_size=4
-        growth_rate=32
-        drop_rate=0
-        memory_efficient=False
-        
-
-        # First convolution
-        self.features = nn.Sequential(OrderedDict([
-            ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2,
-                                padding=3, bias=False)),
-            ('norm0', nn.BatchNorm2d(num_init_features)),
-            ('relu0', nn.ReLU(inplace=True)),
-            ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
-        ]))
-
-        # Each denseblock
-        num_features = num_init_features
-        for i, num_layers in enumerate(block_config):
-            block = _DenseBlock(
-                num_layers=num_layers,
-                num_input_features=num_features,
-                bn_size=bn_size,
-                growth_rate=growth_rate,
-                drop_rate=drop_rate,
-                memory_efficient=memory_efficient
-            )
-            self.features.add_module('denseblock%d' % (i + 1), block)
-            num_features = num_features + num_layers * growth_rate
-            if i != len(block_config) - 1:
-                trans = _Transition(num_input_features=num_features,
-                                    num_output_features=num_features // 2)
-                self.features.add_module('transition%d' % (i + 1), trans)
-                num_features = num_features // 2
-
-        # Final batch norm
-        self.features.add_module('norm5', nn.BatchNorm2d(num_features))
-        # Linear layer
-        self.model.classifier = nn.Linear(num_features, num_classes)
-
-        # Official init from torch repo.
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight)
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.constant_(m.bias, 0)
-        
+        self.model.classifier = nn.Linear(in_features=1024, out_features=1000, bias=True)
+        self.model.classifier = nn.Linear(1024, num_classes)
     def forward(self, x):
         return self.model(x)
-
 
 class densenet161(nn.Module):
     def __init__(self, num_classes=18):
-        super(densenet161, self).__init__()
+        super().__init__()
+
         self.model = torchvision.models.densenet161(pretrained=True)
-        num_init_features = 96
-        block_config=(6, 12, 36, 24)
-        bn_size=4
-        growth_rate=48
-        drop_rate=0
-        memory_efficient=False
-        
-
-        # First convolution
-        self.features = nn.Sequential(OrderedDict([
-            ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2,
-                                padding=3, bias=False)),
-            ('norm0', nn.BatchNorm2d(num_init_features)),
-            ('relu0', nn.ReLU(inplace=True)),
-            ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
-        ]))
-
-        # Each denseblock
-        num_features = num_init_features
-        for i, num_layers in enumerate(block_config):
-            block = _DenseBlock(
-                num_layers=num_layers,
-                num_input_features=num_features,
-                bn_size=bn_size,
-                growth_rate=growth_rate,
-                drop_rate=drop_rate,
-                memory_efficient=memory_efficient
-            )
-            self.features.add_module('denseblock%d' % (i + 1), block)
-            num_features = num_features + num_layers * growth_rate
-            if i != len(block_config) - 1:
-                trans = _Transition(num_input_features=num_features,
-                                    num_output_features=num_features // 2)
-                self.features.add_module('transition%d' % (i + 1), trans)
-                num_features = num_features // 2
-
-        # Final batch norm
-        self.features.add_module('norm5', nn.BatchNorm2d(num_features))
-        # Linear layer
-        self.model.classifier = nn.Linear(num_features, num_classes)
-
-        # Official init from torch repo.
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight)
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.constant_(m.bias, 0)
-        
+        self.model.classifier = nn.Linear(in_features=2208, out_features=1000, bias=True)
+        self.model.classifier = nn.Linear(2208, num_classes)
     def forward(self, x):
         return self.model(x)
-
 
 class densenet169(nn.Module):
     def __init__(self, num_classes=18):
-        super(densenet169, self).__init__()
+        super().__init__()
+
         self.model = torchvision.models.densenet169(pretrained=True)
-        num_init_features = 64
-        block_config=(6, 12, 32, 32)
-        bn_size=4
-        growth_rate=32
-        drop_rate=0
-        memory_efficient=False
-        
-
-        # First convolution
-        self.features = nn.Sequential(OrderedDict([
-            ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2,
-                                padding=3, bias=False)),
-            ('norm0', nn.BatchNorm2d(num_init_features)),
-            ('relu0', nn.ReLU(inplace=True)),
-            ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
-        ]))
-
-        # Each denseblock
-        num_features = num_init_features
-        for i, num_layers in enumerate(block_config):
-            block = _DenseBlock(
-                num_layers=num_layers,
-                num_input_features=num_features,
-                bn_size=bn_size,
-                growth_rate=growth_rate,
-                drop_rate=drop_rate,
-                memory_efficient=memory_efficient
-            )
-            self.features.add_module('denseblock%d' % (i + 1), block)
-            num_features = num_features + num_layers * growth_rate
-            if i != len(block_config) - 1:
-                trans = _Transition(num_input_features=num_features,
-                                    num_output_features=num_features // 2)
-                self.features.add_module('transition%d' % (i + 1), trans)
-                num_features = num_features // 2
-
-        # Final batch norm
-        self.features.add_module('norm5', nn.BatchNorm2d(num_features))
-        # Linear layer
-        self.model.classifier = nn.Linear(num_features, num_classes)
-
-        # Official init from torch repo.
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight)
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.constant_(m.bias, 0)
-        
+        self.model.classifier = nn.Linear(in_features=1664, out_features=1000, bias=True)
+        self.model.classifier = nn.Linear(1664, num_classes)
     def forward(self, x):
         return self.model(x)
-
 
 class densenet201(nn.Module):
     def __init__(self, num_classes=18):
-        super(densenet201, self).__init__()
+        super().__init__()
+
         self.model = torchvision.models.densenet201(pretrained=True)
-        num_init_features = 64
-        block_config=(6, 12, 48, 32)
-        bn_size=4
-        growth_rate=32
-        drop_rate=0
-        memory_efficient=False
-        
-
-        # First convolution
-        self.features = nn.Sequential(OrderedDict([
-            ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2,
-                                padding=3, bias=False)),
-            ('norm0', nn.BatchNorm2d(num_init_features)),
-            ('relu0', nn.ReLU(inplace=True)),
-            ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
-        ]))
-
-        # Each denseblock
-        num_features = num_init_features
-        for i, num_layers in enumerate(block_config):
-            block = _DenseBlock(
-                num_layers=num_layers,
-                num_input_features=num_features,
-                bn_size=bn_size,
-                growth_rate=growth_rate,
-                drop_rate=drop_rate,
-                memory_efficient=memory_efficient
-            )
-            self.features.add_module('denseblock%d' % (i + 1), block)
-            num_features = num_features + num_layers * growth_rate
-            if i != len(block_config) - 1:
-                trans = _Transition(num_input_features=num_features,
-                                    num_output_features=num_features // 2)
-                self.features.add_module('transition%d' % (i + 1), trans)
-                num_features = num_features // 2
-
-        # Final batch norm
-        self.features.add_module('norm5', nn.BatchNorm2d(num_features))
-        # Linear layer
-        self.model.classifier = nn.Linear(num_features, num_classes)
-
-        # Official init from torch repo.
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight)
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.constant_(m.bias, 0)
-        
+        self.model.classifier = nn.Linear(in_features=1920, out_features=1000, bias=True)
+        self.model.classifier = nn.Linear(1920, num_classes)
     def forward(self, x):
         return self.model(x)
 
 
-## feature 계산에 필요한 denseblock, denselayer, transition
-class _DenseBlock(nn.ModuleDict):
-    _version = 2
+## Efficientnet
+class efficientnet_b0(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.model = EfficientNet.from_pretrained('efficientnet-b0',num_classes=num_classes)
 
-    def __init__(self, num_layers, num_input_features, bn_size, growth_rate, drop_rate, memory_efficient=False):
-        super(_DenseBlock, self).__init__()
-        for i in range(num_layers):
-            layer = _DenseLayer(
-                num_input_features + i * growth_rate,
-                growth_rate=growth_rate,
-                bn_size=bn_size,
-                drop_rate=drop_rate,
-                memory_efficient=memory_efficient,
-            )
-            self.add_module('denselayer%d' % (i + 1), layer)
+    def forward(self, x):
+        
+        return self.model(x)
 
-    def forward(self, init_features):
-        features = [init_features]
-        for name, layer in self.items():
-            new_features = layer(features)
-            features.append(new_features)
-        return torch.cat(features, 1)
+class efficientnet_b1(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.model = EfficientNet.from_pretrained('efficientnet-b1',num_classes=num_classes)
 
-class _DenseLayer(nn.Module):
-    def __init__(self, num_input_features, growth_rate, bn_size, drop_rate, memory_efficient=False):
-        super(_DenseLayer, self).__init__()
-        self.add_module('norm1', nn.BatchNorm2d(num_input_features)),
-        self.add_module('relu1', nn.ReLU(inplace=True)),
-        self.add_module('conv1', nn.Conv2d(num_input_features, bn_size *
-                                           growth_rate, kernel_size=1, stride=1,
-                                           bias=False)),
-        self.add_module('norm2', nn.BatchNorm2d(bn_size * growth_rate)),
-        self.add_module('relu2', nn.ReLU(inplace=True)),
-        self.add_module('conv2', nn.Conv2d(bn_size * growth_rate, growth_rate,
-                                           kernel_size=3, stride=1, padding=1,
-                                           bias=False)),
-        self.drop_rate = float(drop_rate)
-        self.memory_efficient = memory_efficient
+    def forward(self, x):
+        
+        return self.model(x)
 
-    def bn_function(self, inputs):
-        # type: (List[Tensor]) -> Tensor
-        concated_features = torch.cat(inputs, 1)
-        bottleneck_output = self.conv1(self.relu1(self.norm1(concated_features)))  # noqa: T484
-        return bottleneck_output
+class efficientnet_b2(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.model = EfficientNet.from_pretrained('efficientnet-b2',num_classes=num_classes)
+
+    def forward(self, x):
+        
+        return self.model(x)
+
+class efficientnet_b3(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.model = EfficientNet.from_pretrained('efficientnet-b3',num_classes=num_classes)
+
+    def forward(self, x):
+        
+        return self.model(x)
+
+class efficientnet_b4(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.model = EfficientNet.from_pretrained('efficientnet-b4',num_classes=num_classes)
+
+    def forward(self, x):
+        
+        return self.model(x)
+
+class efficientnet_b5(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.model = EfficientNet.from_pretrained('efficientnet-b5',num_classes=num_classes)
+
+    def forward(self, x):
+        
+        return self.model(x)
+
+class efficientnet_b6(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.model = EfficientNet.from_pretrained('efficientnet-b6',num_classes=num_classes)
+
+    def forward(self, x):
+        
+        return self.model(x)
+
+class efficientnet_b7(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.model = EfficientNet.from_pretrained('efficientnet-b7',num_classes=num_classes)
+
+    def forward(self, x):
+        
+        return self.model(x)
+
+
+
+class googlenet(nn.Module):
     
-    def forward(self, input):  # noqa: F811
-        if isinstance(input, Tensor):
-            prev_features = [input]
-        else:
-            prev_features = input
+    def __init__(self, num_classes):
+        super().__init__()
+        self.model = torchvision.models.googlenet(pretrained=True)
+        self.model.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.model.dropout = nn.Dropout(0.2)
+        self.model.fc = nn.Linear(1024, num_classes)
+    def forward(self, x):
+        return self.model(x)
 
-        if self.memory_efficient and self.any_requires_grad(prev_features):
-            if torch.jit.is_scripting():
-                raise Exception("Memory Efficient not supported in JIT")
-
-            bottleneck_output = self.call_checkpoint_bottleneck(prev_features)
-        else:
-            bottleneck_output = self.bn_function(prev_features)
-
-        new_features = self.conv2(self.relu2(self.norm2(bottleneck_output)))
-        if self.drop_rate > 0:
-            new_features = F.dropout(new_features, p=self.drop_rate,
-                                     training=self.training)
-        return new_features
-
-class _Transition(nn.Sequential):
-    def __init__(self, num_input_features, num_output_features):
-        super(_Transition, self).__init__()
-        self.add_module('norm', nn.BatchNorm2d(num_input_features))
-        self.add_module('relu', nn.ReLU(inplace=True))
-        self.add_module('conv', nn.Conv2d(num_input_features, num_output_features,
-                                          kernel_size=1, stride=1, bias=False))
-        self.add_module('pool', nn.AvgPool2d(kernel_size=2, stride=2))
