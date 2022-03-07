850d09b5084f9d4e7165a14ed094b4730ffb1f40
홍수연_T3240 <sparklenow@naver.com>
2022-03-07 19:52:55 +0900

    Revert "OLD class dataset 추가 학습"
    This reverts commit 534439855cb017f62c95183ec48b4d28c8d5b8f9.

--- baseline/dataset.py
+++ baseline/dataset.py
@@ -9,8 +9,7 @@ import torch
 from PIL import Image
 from torch.utils.data import Dataset, Subset, random_split
 from torchvision.transforms import Resize, ToTensor, Normalize, Compose, CenterCrop, ColorJitter, RandomHorizontalFlip, RandomPerspective
-from sklearn.model_selection import KFold
-# from randaugment import RandAugment
+
 IMG_EXTENSIONS = [
     ".jpg", ".JPG", ".jpeg", ".JPEG", ".png",
     ".PNG", ".ppm", ".PPM", ".bmp", ".BMP",
@@ -201,7 +200,7 @@ class MaskBaseDataset(Dataset):
             self.mean = np.mean(sums, axis=0) / 255
             self.std = (np.mean(squared, axis=0) - self.mean ** 2) ** 0.5 / 255
 
-    def set_transform(self, transform, mode='both'):
+    def set_transform(self, transform):
         self.transform = transform
 
     def __getitem__(self, index):
@@ -262,207 +261,7 @@ class MaskBaseDataset(Dataset):
         n_val = int(len(self) * self.val_ratio)
         n_train = len(self) - n_val
         train_set, val_set = random_split(self, [n_train, n_val])
-        data_set_list = []
-        data_set_list.append((0,train_set,val_set)) #fold = 0
-        return data_set_list
-
-    
-    
-
-class MaskBaseDataset_AddData(Dataset):
-    num_classes = 3 * 2 * 3
-
-    _file_names = {
-        "mask1": MaskLabels.MASK,
-        "mask2": MaskLabels.MASK,
-        "mask3": MaskLabels.MASK,
-        "mask4": MaskLabels.MASK,
-        "mask5": MaskLabels.MASK,
-        "incorrect_mask": MaskLabels.INCORRECT,
-        "normal": MaskLabels.NORMAL
-    }
-
-    image_paths = []
-    mask_labels = []
-    gender_labels = []
-    age_labels = []
-    change_section = 0 
-
-
-    def __init__(self, data_dir, mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246), val_ratio=0.2):
-        self.data_dir = data_dir
-        self.mean = mean
-        self.std = std
-        self.val_ratio = val_ratio
-
-        self.setup()
-        self.calc_statistics()
-        self.transform =  Compose([
-            CenterCrop((320, 256)),
-            Resize([512,384], Image.BILINEAR),
-            ToTensor(),
-            Normalize(mean=self.mean, std=self.std),
-            ])
-        self.transform1 = Compose([         
-            CenterCrop((320, 256)),
-            RandomHorizontalFlip(p=1.0), # randomly H_flip images
-            Resize([512,384], Image.BILINEAR),
-            ToTensor(),
-            Normalize(mean=self.mean, std=self.std),
-            AddGaussianNoise(), 
-        ])
-        self.transform2 = Compose([         
-            CenterCrop((320, 256)),
-            Resize([512,384], Image.BILINEAR),
-            ColorJitter(brightness=0.1), # randomly change color space
-            RandomPerspective(distortion_scale=0.3, p=0.2),
-            ToTensor(),
-            Normalize(mean=self.mean, std=self.std),
-        ])
-
-    def setup(self):
-        profiles = os.listdir(self.data_dir)
-        old = []
-        for profile in profiles:
-            if profile.startswith("."):  # "." 로 시작하는 파일은 무시합니다
-                continue
-            
-            img_folder = os.path.join(self.data_dir, profile)
-            for file_name in os.listdir(img_folder):
-                _file_name, ext = os.path.splitext(file_name)
-                if _file_name not in self._file_names:  # "." 로 시작하는 파일 및 invalid 한 파일들은 무시합니다
-                    continue
-
-                img_path = os.path.join(self.data_dir, profile, file_name)  # (resized_data, 000004_male_Asian_54, mask1.jpg)
-                mask_label = self._file_names[_file_name]
-
-                id, gender, race, age = profile.split("_")
-                gender_label = GenderLabels.from_str(gender)
-                age_label = AgeLabels.from_number(age)
-
-                self.image_paths.append(img_path)
-                self.mask_labels.append(mask_label)
-                self.gender_labels.append(gender_label)
-                self.age_labels.append(age_label)
-                self.change_section += 1
-
-                # Old case: 
-                if age_label is AgeLabels.OLD:
-                    old.append([img_path,mask_label,gender_label,age_label])
-
-        for i_path, i_mask, i_gender, i_age in old:
-            self.image_paths.append(i_path)
-            self.mask_labels.append(i_mask)
-            self.gender_labels.append(i_gender)
-            self.age_labels.append(i_age)
-        for i_path, i_mask, i_gender, i_age in old:
-            self.image_paths.append(i_path)
-            self.mask_labels.append(i_mask)
-            self.gender_labels.append(i_gender)
-            self.age_labels.append(i_age)
-
-
-    def calc_statistics(self):
-        has_statistics = self.mean is not None and self.std is not None
-        if not has_statistics:
-            print("[Warning] Calculating statistics... It can take a long time depending on your CPU machine")
-            sums = []
-            squared = []
-            for image_path in self.image_paths[:3000]:
-                image = np.array(Image.open(image_path)).astype(np.int32)
-                sums.append(image.mean(axis=(0, 1)))
-                squared.append((image ** 2).mean(axis=(0, 1)))
-
-            self.mean = np.mean(sums, axis=0) / 255
-            self.std = (np.mean(squared, axis=0) - self.mean ** 2) ** 0.5 / 255
-
-      
-    def __getitem__(self, index):
-        # assert self.transform is not None, ".set_tranform 메소드를 이용하여 transform 을 주입해주세요"
-
-        image = self.read_image(index)
-        mask_label = self.get_mask_label(index)
-        gender_label = self.get_gender_label(index)
-        age_label = self.get_age_label(index)
-        multi_class_label = self.encode_multi_class(mask_label, gender_label, age_label)
-    
-        if index <= self.change_section:
-            image_transform = self.transform(image)
-        elif index > self.change_section and int((len(self) - self.change_section) /2 + self.change_section) >= index :
-            image_transform = self.transform1(image)
-        else:
-            image_transform = self.transform2(image)
-
-        return image_transform, multi_class_label
-
-
-    def __len__(self):
-        return len(self.image_paths)
-
-    def get_mask_label(self, index) -> MaskLabels:
-        return self.mask_labels[index]
-
-    def get_gender_label(self, index) -> GenderLabels:
-        return self.gender_labels[index]
-
-    def get_age_label(self, index) -> AgeLabels:
-        return self.age_labels[index]
-
-    def read_image(self, index):
-        image_path = self.image_paths[index]
-        return Image.open(image_path)
-
-    @staticmethod
-    def encode_multi_class(mask_label, gender_label, age_label) -> int:
-        return mask_label * 6 + gender_label * 3 + age_label
-
-    @staticmethod
-    def decode_multi_class(multi_class_label) -> Tuple[MaskLabels, GenderLabels, AgeLabels]:
-        mask_label = (multi_class_label // 6) % 3
-        gender_label = (multi_class_label // 3) % 2
-        age_label = multi_class_label % 3
-        return mask_label, gender_label, age_label
-
-    @staticmethod
-    def denormalize_image(image, mean, std):
-        img_cp = image.copy()
-        img_cp *= std
-        img_cp += mean
-        img_cp *= 255.0
-        img_cp = np.clip(img_cp, 0, 255).astype(np.uint8)
-        return img_cp
-
-    def split_dataset(self) : #-> Tuple[Subset, Subset]:
-        """
-        데이터셋을 train 과 val 로 나눕니다,
-        pytorch 내부의 torch.utils.data.random_split 함수를 사용하여
-        torch.utils.data.Subset 클래스 둘로 나눕니다.
-        구현이 어렵지 않으니 구글링 혹은 IDE (e.g. pycharm) 의 navigation 기능을 통해 코드를 한 번 읽어보는 것을 추천드립니다^^
-        """
-        n_val = int(len(self) * self.val_ratio)
-        n_train = len(self) - n_val
-        train_set, val_set = random_split(self, [n_train, n_val])
         return train_set, val_set
-
-
-
-class kfold(MaskBaseDataset):
-    
-    def __init__(self, data_dir, mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246), val_ratio=0.2):
-        self.indices = defaultdict(list)
-        super().__init__(data_dir, mean, std, val_ratio)
-    
-    # Define the K-fold Cross Validator
-    def split_dataset(self):
-        kfold = KFold(n_splits=5, shuffle=True)
-        # K-fold Cross Validation model evaluation
-        data_set_list = []
-        for fold, (train_ids, val_ids) in enumerate(kfold.split(self)):
-            # Sample elements randomly from a given list of ids, no replacement.
-            train_set_list = Subset(self,train_ids)
-            val_set_list = Subset(self,val_ids)
-            data_set_list.append((fold,train_set_list,val_set_list))
-        return data_set_list
 
 
 class MaskSplitByProfileDataset(MaskBaseDataset):
@@ -520,10 +319,7 @@ class MaskSplitByProfileDataset(MaskBaseDataset):
                     cnt += 1
 
     def split_dataset(self) -> List[Subset]:
-        test_set,val_set = [Subset(self, indices) for phase, indices in self.indices.items()]
-        data_set_list = []
-        data_set_list.append((0,test_set,val_set))
-        return data_set_list
+        return [Subset(self, indices) for phase, indices in self.indices.items()]
 
 class TestDataset(Dataset):
     def __init__(self, img_paths, resize, mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246)):
@@ -544,6 +340,3 @@ class TestDataset(Dataset):
 
     def __len__(self):
         return len(self.img_paths)
-
-
-
